0b6523c84f8d08a2e1bbccf006552150f7717d46
root <SivaNarayana150>
2023-10-17 05:28:15 +0000

    api 3 sqlite error

--- app.js
+++ app.js
@@ -1,177 +1,160 @@
-//Importing Modules
-
 const express = require("express");
 const path = require("path");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
-const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const dbPath = path.join(__dirname, "twitterClone.db");
-
 const app = express();
 
 app.use(express.json());
 
-let db = null;
+let db = "";
 
-//initializing Database And Server
 const initializeDatabaseAndServer = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
-
     app.listen(3001, () => {
-      console.log("Server Running Successfully At http://localhost:3001/");
+      console.log("Server running Successfully at http://localhost:3001/");
     });
-  } catch (error) {
-    console.log(`DB ERROR ${error.message}`);
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
     process.exit(1);
   }
 };
 
 initializeDatabaseAndServer();
 
-//Authentication Middle wear
+const convertResponseObjectUser = (dbObject) => {
+  return {
+    userId: dbObject.user_id,
+    username: dbObject.username,
+    password: dbObject.password,
+    name: dbObject.name,
+    gender: dbObject.gender,
+  };
+};
 
-const authenticationToken = (request, response, next) => {
-  let jsonWebToken;
+const Authentication = (request, response, next) => {
+  let jwtTokenCheck;
   const authHeader = request.headers["authorization"];
-
   if (authHeader !== undefined) {
-    jsonWebToken = authHeader.split(" ")[1];
+    jwtTokenCheck = authHeader.split(" ")[1];
   }
 
-  if (jsonWebToken === undefined) {
-    response.status(401);
+  if (jwtTokenCheck === undefined) {
+    response.status(400);
+    // If the JWT token is not provided by the user or an invalid JWT token is provided
     response.send("Invalid JWT Token");
   } else {
     jsonwebtoken.verify(
-      jsonWebToken,
-      "MY_SECRET_TOKEN",
+      jwtTokenCheck,
+      "MY_SECRET_KEY",
       async (error, payload) => {
         if (error) {
-          response.status(401);
+          response.status(400);
+          //If the JWT token is not provided by the user or an invalid JWT token is provided
           response.send("Invalid JWT Token");
         } else {
-          next();
+          next(); //After successful verification of JWT token, proceed to next middleware or handler
         }
       }
     );
   }
 };
 
-// API 1 Register
+//API one
 
 app.post("/register/", async (request, response) => {
+  const { userId } = request.params;
   const { username, password, name, gender } = request.body;
-  //Checking Whether The user Exists
+  const fetchUserExistQuery = `SELECT * FROM user WHERE username='${username}';`;
+  const userFetchQueryResponse = await db.get(fetchUserExistQuery);
 
-  const QueryToObtainUser = `SELECT * FROM user WHERE username='${username}';`;
-
-  const runTheQuery = await db.get(QueryToObtainUser);
-
-  //Scenario 1
-  if (runTheQuery === undefined) {
+  if (userFetchQueryResponse === undefined) {
+    //If the registrant provides a password with less than 6 characters
     if (password.length > 5) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const queryToCreateUser = `INSERT INTO user (username, password, name, gender)
-      VALUES(
-          '${username}',
-          '${hashedPassword}',
-          '${name}',
-          '${gender}'
-      );`;
+      const hashPassword = await bcrypt.hash(password, 10);
 
-      const responseResult = await db.run(queryToCreateUser);
-      response.status(200);
+      const registerWithUserQuery = `INSERT INTO user (username,password,name,gender)
+      
+      VALUES(
+            '${username}',
+            '${hashPassword}',
+            '${name}',
+            '${gender}'
+
+      );`;
+      const registerResponse = await db.run(registerWithUserQuery);
+      //Successful registration of the registrant
+      response.status(400);
       response.send("User created successfully");
     } else {
       response.status(400);
       response.send("Password is too short");
     }
   } else {
+    // if user already exists
     response.status(400);
+
     response.send("User already exists");
   }
 });
 
-//API 2 login
+// API 2
 
 app.post("/login/", async (request, response) => {
   const { username, password } = request.body;
-
-  const findingUserQuery = `SELECT * FROM user WHERE username='${username}';`;
-  const getResult = await db.get(findingUserQuery);
-
-  if (getResult === undefined) {
-    response.status(400);
-    response.send("Invalid user");
-  } else {
-    const isPasswordMatched = await bcrypt.compare(
+  const fetchUserExistQuery = `SELECT * FROM user WHERE username='${username}';`;
+  const userFetchQueryResponse = await db.get(fetchUserExistQuery);
+  const userLogin = convertResponseObjectUser(userFetchQueryResponse);
+  if (userLogin.username !== undefined) {
+    const payLoad = userLogin.username;
+    const checkHashedPassword = await bcrypt.compare(
       password,
-      getResult.password
+      userLogin.password
     );
-    if (isPasswordMatched === true) {
-      const jwtToken = jsonwebtoken.sign(getResult, "MY_SECRET_TOKEN");
-      response.send({ jwtToken });
+    if (checkHashedPassword === true) {
+      //Successful login of the user
+      const jwtToken = jsonwebtoken.sign(payLoad, "MY_SECRET_KEY");
+      response.send({ jwtToken }); //Return the JWT Token
     } else {
+      //If the user provides an incorrect password
       response.status(400);
       response.send("Invalid password");
     }
+  } else {
+    //If the user doesn't have a Twitter account
+    response.status(400);
+    response.send("Invalid user");
   }
 });
 
-const outPutResult = (dbObject) => {
+//API 3
+
+const convertApi3 = (dbObject) => {
   return {
     username: dbObject.username,
     tweet: dbObject.tweet,
-    dateTime: dbObject.date_time,
+    dateTime: dbObject.dateTime,
   };
 };
 
-// API 3 Returns the latest tweets of people whom the user follows. Return 4 tweets at a time
-app.get(
-  "/user/tweets/feed/",
-  authenticationToken,
-  async (request, response) => {
-    const { userId } = request.params;
-    const getTweetsQuery = `SELECT  user.username ,
-    tweet.tweet  ,tweet.date_time AS dateTime
-    FROM follower INNER JOIN tweet ON follower.following_user_id=tweet.user_id INNER JOIN user ON  tweet.user_id = user.user_id
-    WHERE follower.follower_user_id=${userId}
-    ORDER BY
-    tweet.date_time DESC
-    LIMIT 4;`;
-
-    const responseResult = await db.all(getTweetsQuery);
-    response.send(responseResult.map((each) => outPutResult(each)));
-  }
-);
-
-//API 4
-app.get("/user/following/", authenticationToken, async (request, response) => {
+app.get("/user/tweets/feed/", Authentication, async (request, response) => {
   const { userId } = request.params;
-  const obtainQuery = `SELECT DISTINCT name from follower INNER JOIN user ON user.user_id = follower.follower_user_id 
-  WHERE follower_user_id='${userId}' ;`;
-  const getResult = await db.all(obtainQuery);
-  response.send(getResult);
+  const fetchUserLatestTweets = `SELECT user.username AS username, tweet.tweet AS tweet, tweet.date_time AS dateTime
+     FROM follower INNER JOIN tweet ON follower.following_user_id=tweet.user_id INNER JOIN user ON  tweet.user_id = user.user_id
+ WHERE follower.follower_user_id=${userId}
+ ORDER BY tweet.date_time DESC
+ LIMIT 4;`;
+  const getResultResponse = await db.all(fetchUserLatestTweets);
+  response.send(getResultResponse.map((each) => convertApi3(each)));
 });
-
-//API 5
-
-app.get("/user/followers/", authenticationToken, async (request, response) => {
-  const { userId } = request.params;
-  const obtainQuery = `SELECT DISTINCT name from follower INNER JOIN user ON user.user_id = follower.follower_user_id 
-  WHERE following_user_id='${userId}' ;`;
-  const getResult = await db.all(obtainQuery);
-  response.send(getResult);
-});
-
-//API 6
 
 module.exports = app;
