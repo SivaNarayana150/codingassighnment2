f543b0c99bb653c0052a13c9df4befc7b66128e9
root <SivaNarayana150>
2023-10-18 03:52:48 +0000

    api 3 on

--- app.js
+++ app.js
@@ -31,130 +31,109 @@ const initializeDatabaseAndServer = async () => {
 
 initializeDatabaseAndServer();
 
-const convertResponseObjectUser = (dbObject) => {
+const convertDataBaseObject = (dbObject) => {
   return {
     userId: dbObject.user_id,
+    name: dbObject.name,
     username: dbObject.username,
     password: dbObject.password,
-    name: dbObject.name,
-    gender: dbObject.gender,
   };
 };
 
+//Authentication
+
 const Authentication = (request, response, next) => {
-  let jwtTokenCheck;
+  const { tweetId } = request.params;
+  const { tweet } = request.body;
+
+  let JWTToken = "";
+
   const authHeader = request.headers["authorization"];
   if (authHeader !== undefined) {
-    jwtTokenCheck = authHeader.split(" ")[1];
+    JWTToken = authHeader.split(" ")[1];
   }
-
-  if (jwtTokenCheck === undefined) {
-    response.status(400);
-    // If the JWT token is not provided by the user or an invalid JWT token is provided
+  if (JWTToken === undefined) {
+    response.status(401);
     response.send("Invalid JWT Token");
   } else {
-    jsonwebtoken.verify(
-      jwtTokenCheck,
-      "MY_SECRET_KEY",
-      async (error, payload) => {
-        if (error) {
-          response.status(400);
-          //If the JWT token is not provided by the user or an invalid JWT token is provided
-          response.send("Invalid JWT Token");
-        } else {
-          next(); //After successful verification of JWT token, proceed to next middleware or handler
-        }
+    jsonwebtoken.verify(JWTToken, "MY_SECRET_KEY", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.tweetId = tweetId;
+        request.tweet = tweet;
+        request.username = payload;
+
+        next();
       }
-    );
+    });
   }
 };
 
-//API one
-
+//API 1 register
 app.post("/register/", async (request, response) => {
-  const { userId } = request.params;
   const { username, password, name, gender } = request.body;
-  const fetchUserExistQuery = `SELECT * FROM user WHERE username='${username}';`;
-  const userFetchQueryResponse = await db.get(fetchUserExistQuery);
-
-  if (userFetchQueryResponse === undefined) {
-    //If the registrant provides a password with less than 6 characters
+  const fetchUserExistenceQuery = `SELECT * FROM user WHERE username='${username}';`;
+  const registerDetails = await db.get(fetchUserExistenceQuery);
+  if (registerDetails === undefined) {
     if (password.length > 5) {
-      const hashPassword = await bcrypt.hash(password, 10);
+      const hashingPassword = await bcrypt.hash(password, 10);
+      const userRegistration = `INSERT INTO user (username, password, name, gender)
+          VALUES ('${username}',
+          '${hashingPassword}',
+          '${name}',
+          '${gender}');`;
+      await db.run(userRegistration);
 
-      const registerWithUserQuery = `INSERT INTO user (username,password,name,gender)
-      
-      VALUES(
-            '${username}',
-            '${hashPassword}',
-            '${name}',
-            '${gender}'
-
-      );`;
-      const registerResponse = await db.run(registerWithUserQuery);
-      //Successful registration of the registrant
-      response.status(400);
+      response.status(200);
       response.send("User created successfully");
     } else {
       response.status(400);
       response.send("Password is too short");
     }
   } else {
-    // if user already exists
     response.status(400);
-
     response.send("User already exists");
   }
 });
 
-// API 2
+//API 2
 
 app.post("/login/", async (request, response) => {
   const { username, password } = request.body;
-  const fetchUserExistQuery = `SELECT * FROM user WHERE username='${username}';`;
-  const userFetchQueryResponse = await db.get(fetchUserExistQuery);
-  const userLogin = convertResponseObjectUser(userFetchQueryResponse);
-  if (userLogin.username !== undefined) {
-    const payLoad = userLogin.username;
-    const checkHashedPassword = await bcrypt.compare(
+  const fetchUserExistence = `SELECT * FROM user WHERE username='${username}';`;
+  const responseUserDetails = await db.get(fetchUserExistence);
+
+  if (responseUserDetails === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const LoginUserDetails = convertDataBaseObject(responseUserDetails);
+
+    const isPasswordMatched = await bcrypt.compare(
       password,
-      userLogin.password
+      LoginUserDetails.password
     );
-    if (checkHashedPassword === true) {
-      //Successful login of the user
-      const jwtToken = jsonwebtoken.sign(payLoad, "MY_SECRET_KEY");
-      response.send({ jwtToken }); //Return the JWT Token
+    if (isPasswordMatched === true) {
+      const payload = LoginUserDetails.username;
+
+      console.log(payload);
+      const jwtToken = jsonwebtoken.sign(payload, "MY_SECRET_KEY");
+      response.send({ jwtToken });
     } else {
-      //If the user provides an incorrect password
       response.status(400);
       response.send("Invalid password");
     }
-  } else {
-    //If the user doesn't have a Twitter account
-    response.status(400);
-    response.send("Invalid user");
   }
 });
 
 //API 3
 
-const convertApi3 = (dbObject) => {
-  return {
-    username: dbObject.username,
-    tweet: dbObject.tweet,
-    dateTime: dbObject.dateTime,
-  };
-};
-
 app.get("/user/tweets/feed/", Authentication, async (request, response) => {
-  const { userId } = request.params;
-  const fetchUserLatestTweets = `SELECT user.username AS username, tweet.tweet AS tweet, tweet.date_time AS dateTime
-     FROM follower INNER JOIN tweet ON follower.following_user_id=tweet.user_id INNER JOIN user ON  tweet.user_id = user.user_id
- WHERE follower.follower_user_id=${userId}
- ORDER BY tweet.date_time DESC
- LIMIT 4;`;
-  const getResultResponse = await db.all(fetchUserLatestTweets);
-  response.send(getResultResponse.map((each) => convertApi3(each)));
+  const { username } = request;
+  const FetchUser_id = `SELECT user_id FROM user WHERE username='${username}';`;
+  const user_id = await db.get(FetchUser_id);
 });
 
 module.exports = app;
